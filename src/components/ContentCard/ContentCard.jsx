9de59d50e0caeeee2ccf7a010de9e5367e4c3359
KeanuH95 <keanu@buildwithnewco.com>
2023-09-18 16:19:34 -0400

    work page

--- src/components/ContentCard/ContentCard.jsx
+++ src/components/ContentCard/ContentCard.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from './ContentCard.module.scss';
-import { useMediaQuery } from "react-responsive";
 
-function ContentCard({cardTitle, cardSubtitle, cols}) {
-    const isMobile = useMediaQuery({ query: "(max-width: 640px)" });
-    const cardWidth = isMobile ? 100 : 1/cols * 100;
-
+function ContentCard({cardTitle, cardImage, cardDesc, cardLink}) {
     return (
-        <div className={styles.card} style={{width: `${cardWidth}%`}}>
+        <div className={styles.card} >
             <div className={styles.cardContent}>
                 <h3 className={styles.cardTitle}>{cardTitle}</h3>
-                <h4 className={styles.cardSubtitle}>{cardSubtitle}</h4>
+                { !!cardLink && (
+                    <Link to={cardLink} target="_blank" rel="noopener noreferrer" className={styles.cardImage}>
+                        <img src={cardImage} alt="cardImage" />
+                    </Link>
+                )}
+                {!cardLink && (
+                    <Link to={"#"} className={styles.cardImage}>
+                        <img src={cardImage} alt="cardImage" />
+                    </Link>
+                )}
+                <p className={styles.cardDesc}>{cardDesc}</p>
             </div>
         </div>
     );
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,10 +1,51 @@
 import React from "react";
-import styles from './Work.module.scss';
+import styles from "./Work.module.scss";
+import OarHealthLogo from "../../images/OarHealthLogo.jpg";
+import ShoeboxLogo from "../../images/ShoeboxLogo.png";
+import KitLogo from "../../images/KitLogo.png";
+import JPHDesignLogo from "../../images/JPHDesignLogo.png";
+import KHLogo from "../../images/KHLogo.png";
+import ContentCard from "../ContentCard/ContentCard";
+import BGInitials from "../BGInitials/BGInitials";
 
 function Work() {
   return (
-    <div>
-      Work
+    <div className={styles.workWrapper}>
+      <div className={styles.workContent}>
+        <h1>Companies and Projects I've Helped Build</h1>
+        <div className={styles.workProjects}>
+          <ContentCard
+            cardTitle={"Oar Health"}
+            cardImage={OarHealthLogo}
+            cardDesc={"A service that provides people who are ready to change their relationship with drinking access to medical support and science-based solutions."}
+            cardLink={"https://www.oarhealth.com/"}
+          />
+          <ContentCard
+            cardTitle={"Shoebox"}
+            cardImage={ShoeboxLogo}
+            cardDesc={"The smartest home base for your sports cards. Scan your cards to discover your next grail, follow market pricing, and showcase your collection."}
+            cardLink={"https://myshoebox.app/"}
+          />
+          <ContentCard
+            cardTitle={"Kit Renovation"}
+            cardImage={KitLogo}
+            cardDesc={"A stress-free, fixed-price bathroom remodel including design, materials, project management and construction."}
+            cardLink={"https://kitreno.com/"}
+          />
+          <ContentCard
+            cardTitle={"JPH Design"}
+            cardImage={JPHDesignLogo}
+            cardDesc={"A Montreal based retail design company that offers concept development, 3D renders, CAD digitalization and more."}
+            cardLink={"https://jph-design.com/"}
+          />
+          <ContentCard
+            cardTitle={"Stealth Startup"}
+            cardImage={KHLogo}
+            cardDesc={"A music startup that empowers both artists and fans. This project had me developing a Java backend and diving into a wide range of AWS services."}
+          />
+        </div>
+      </div>
+      <BGInitials />
     </div>
   );
 }
