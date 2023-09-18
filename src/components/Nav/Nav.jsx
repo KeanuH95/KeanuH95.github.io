fcb1dbc34655f2ceb72e923495a78057e5f67690
KeanuH95 <keanu@buildwithnewco.com>
2023-09-18 18:56:03 -0400

    switch to Links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import styles from "./Nav.module.scss";
 import routes from "../../utils/routes";
+import { Link } from "react-router-dom";
 
 function Nav () {
     return (
         <nav className={styles.navBar}>
             <h1><a href={routes.HOME_ROUTE}>KH</a></h1>
             <ul>
-                <li><a href={routes.WORK_ROUTE}>Work</a></li>
-                <li><a href={routes.SKILLS_ROUTE}>Skills</a></li>
-                <li><a href={routes.EXPERIENCE_ROUTE}>Experience</a></li>
-                <li><a href={routes.CONTACT_ROUTE}>Contact</a></li>
+                <li><Link to={routes.WORK_ROUTE}>Work</Link></li>
+                <li><Link to={routes.SKILLS_ROUTE}>Skills</Link></li>
+                <li><Link to={routes.EXPERIENCE_ROUTE}>Experience</Link></li>
+                <li><Link to={routes.CONTACT_ROUTE}>Contact</Link></li>
             </ul>
         </nav>
     )
